5c4558c8501249d5fba7146c14b4bac2818c7d05
InVanSav <savickijivan11@gmail.com>
2023-02-19 03:12:12 +1000

    Implemented HttpPut

--- inferback/Controllers/Project/HttpPutProject.cs
+++ inferback/Controllers/Project/HttpPutProject.cs
@@ -1,5 +1,29 @@
+using Inferback.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace inferback.Controllers.Project; 
 
-public class HttpPutProject {
+[ApiController]
+[Route("api/project/[controller]")]
+public class HttpPutProject : ControllerBase {
+    private readonly IProjectService _projectService;
     
+    public HttpPutProject(IProjectService projectService) {
+        _projectService = projectService;
+    }
+    
+    [HttpPut("edit-project/{id}/{entity}")]
+    public async Task<IActionResult> EditProject(int id, Inferback.Domain.Entity.Project entity) {
+        if (id == null || entity == null) {
+            return BadRequest("Request have to include entity");
+        }
+
+        var response = await _projectService.UpdateProject(id, entity);
+
+        if (response.StatusCode == Inferback.Domain.Enum.StatusCode.OK) {
+            return Ok("Object 'Project' edited successfully");
+        }
+
+        return BadRequest("Object 'Project' did not edit");
+    }
 }
--- inferback/Controllers/Report/HttpPutReport.cs
+++ inferback/Controllers/Report/HttpPutReport.cs
@@ -1,5 +1,29 @@
+using Inferback.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace inferback.Controllers.Report; 
 
-public class HttpPutReport {
+[ApiController]
+[Route("api/report/[controller]")]
+public class HttpPutReport : ControllerBase {
+    private readonly IReportService _reportService;
     
+    public HttpPutReport(IReportService reportService) {
+        _reportService = reportService;
+    }
+    
+    [HttpPut("edit-report/{id}/{entity}")]
+    public async Task<IActionResult> EditReport(int id, Inferback.Domain.Entity.Report entity) {
+        if (id == null || entity == null) {
+            return BadRequest("Request have to include entity");
+        }
+
+        var response = await _reportService.UpdateReport(id, entity);
+
+        if (response.StatusCode == Inferback.Domain.Enum.StatusCode.OK) {
+            return Ok("Object 'Report' edited successfully");
+        }
+
+        return BadRequest("Object 'Report' did not edit");
+    }
 }
