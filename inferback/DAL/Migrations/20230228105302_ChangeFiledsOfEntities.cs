2b1e7a354246739c6888f1bff0d7e3d2b96cd8c9
InVanSav <savickijivan11@gmail.com>
2023-02-28 21:33:55 +1000

    Changed database

---
+++ inferback/DAL/Migrations/20230228105302_ChangeFiledsOfEntities.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace inferback.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeFiledsOfEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "path",
+                table: "Reports",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "Descriptions",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "path",
+                table: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "Descriptions");
+        }
+    }
+}
